510df251dd475cd195189b601ac760d00baa76fb
Nemanja-MO <lazarevic.nemanja@gmail.com>
2020-12-20 09:49:10 +0100

    Edit ajax search

--- ajax.php
+++ ajax.php
@@ -14,27 +14,27 @@ mysqli_set_charset($conn, "utf8");
 //Uzimanje vrijednosti iz polja search
 if (isset($_POST['search'])) {
    //Dodjeljivanje prosljeđene vrijednosti promjenljivoj
-   $Name = $_POST['search'];
+   $Name = mysqli_real_escape_string($conn, $_POST['search']);
    //String koji sadrži unesenu vrijednost sa znakom '%' umjesto razmaka. Ovo je dodatni parametar koji unaprijeđuje pretragu pacijenta
    $string = preg_replace('/\s+/', '%', $Name);
    //Upit za pretragu unesenog pojma
-   $Query = "SELECT ID,generalije_pacijenta,kontakt,napomena,naocare_daljina_od,naocare_daljina_os,naocare_blizina_od,naocare_blizina_os,sociva_od,sociva_os FROM pacijenti WHERE generalije_pacijenta LIKE '%$Name%' OR generalije_pacijenta LIKE '$string' OR generalije_pacijenta LIKE '$string%' LIMIT 10";
+   $sql = "SELECT ID,generalije_pacijenta,kontakt,napomena,naocare_daljina_od,naocare_daljina_os,naocare_blizina_od,naocare_blizina_os,sociva_od,sociva_os FROM pacijenti WHERE generalije_pacijenta LIKE '%$Name%' OR generalije_pacijenta LIKE '$string' OR generalije_pacijenta LIKE '$string%' LIMIT 10";
    //Izvršavanje upita
-   $ExecQuery = MySQLi_query($conn, $Query);
+   $result = MySQLi_query($conn, $sql);
    //Kreiranje neuređena liste za prikaz rezultata
    echo '
     <ul>
        ';
    $numRow = 0;
    //Čitanje rezultata upita
-   while ($Result = MySQLi_fetch_array($ExecQuery)) {
+   while ($row = MySQLi_fetch_array($result)) {
       $numRow = 1;
       //Kreiranje stavki neuređene liste.
       //Stavki se dodjeljuje onclick triger koji na "kliknuti" rezultat isti prosljeđuje metodi "fill" koja se nalazi u "js/alertify.min.js"
 ?>
-      <li id="ajaxResults" onclick='fill("<?php echo $Result['ID'] . "#" . $Result['generalije_pacijenta'] . "#" . $Result['kontakt'] . "#" . $Result['napomena'] . "#" . $Result['naocare_daljina_od'] . "#" . $Result['naocare_daljina_os'] . "#" . $Result['naocare_blizina_od'] . "#" . $Result['naocare_blizina_os'] . "#" . $Result['sociva_od'] . "#" . $Result['sociva_os'] ."#"; ?>")'>
-         <a value="<?php echo $Result['ID']; ?>">
-            <?php echo $Result['generalije_pacijenta']; ?>
+      <li id="ajaxResults" onclick='fill("<?php echo $row['ID'] . "#" . $row['generalije_pacijenta'] . "#" . $row['kontakt'] . "#" . $row['napomena'] . "#" . $row['naocare_daljina_od'] . "#" . $row['naocare_daljina_os'] . "#" . $row['naocare_blizina_od'] . "#" . $row['naocare_blizina_os'] . "#" . $row['sociva_od'] . "#" . $row['sociva_os']; ?>")'>
+         <a value="<?php echo $row['ID']; ?>">
+            <?php echo $row['generalije_pacijenta']; ?>
       </li></a>
 <?php
    }
