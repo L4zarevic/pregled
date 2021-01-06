73ede63bbb4681efc91801738f87caba9092c470
Nemanja Lazarević <36665723+L4zarevic@users.noreply.github.com>
2021-01-06 09:59:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -17,8 +17,8 @@
         <div class="container-login100">
             <div class="wrap-login100 p-t-50 p-b-90">
                 <form class="login100-form validate-form flex-sb flex-w" action="../pregled/obrada.php" method="post"><img id="logo" src="../pregled/images/MO.png"><span class="login100-form-title p-b-51"></span>
-                    <div class="wrap-input100 validate-input m-b-16" data-validate="Niste unijeli korisničko ime"><input class="input100" type="text" name="korisnicko_ime" placeholder="Korisničko ime"><span class="focus-input100"></span></div>
-                    <div class="wrap-input100 validate-input m-b-16" data-validate="Niste unijeli lozinku"><input class="input100" type="password" name="lozinka" placeholder="Lozinka"><span class="focus-input100"></span></div>
+                    <div class="wrap-input100 validate-input m-b-16"><input class="input100" type="text" name="korisnicko_ime" placeholder="Korisničko ime"><span class="focus-input100"></span></div>
+                    <div class="wrap-input100 validate-input m-b-16" ><input class="input100" type="password" name="lozinka" placeholder="Lozinka"><span class="focus-input100"></span></div>
                     <div class="container-login100-form-btn m-t-17"><button class="login100-form-btn" type="submit">Prijava</button><br /><br />
                         <?php
                         if (isset($_COOKIE["cica_maca"])) {
@@ -48,7 +48,6 @@
     <script src="vendor/animsition/js/animsition.min.js"></script>
     <script src="vendor/bootstrap/js/popper.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/alertify.min.js"></script>
     <script src='https://www.google.com/recaptcha/api.js' async defer></script>
 </body>
 <!-- „Ko traži, naći će. Ko kuca, otvoriće mu se.“ -->
