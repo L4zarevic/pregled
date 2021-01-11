47cad1df8907c11bdf4c4a72790220b05e99b6ce
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-01-11 22:28:12 +0100

    Replaced double quote with single

--- addExaminationDB.php
+++ addExaminationDB.php
@@ -9,19 +9,19 @@ if (is_null($_SESSION['prijavljen'])) {
 }
 include 'connection.php';
 $korisnik = $_SESSION['prijavljen'];
-$ar = explode("#", $korisnik, 3);
-$ar[1] = rtrim($ar[1], "#");
+$ar = explode('#', $korisnik, 3);
+$ar[1] = rtrim($ar[1], '#');
 $id_korisnika = $ar[0];
 $imeKorisnika = $ar[1];
 $dataBaseName = $ar[2];
 $conn = OpenStoreCon($dataBaseName);
-mysqli_set_charset($conn, "utf8");
+mysqli_set_charset($conn, 'utf8');
 
 $vrsta_pregleda = mysqli_real_escape_string($conn, $_REQUEST['vrsta_pregleda']);
 $id_radnika = mysqli_real_escape_string($conn, $_REQUEST['id_radnika']);
 $ime_prezime_pacijenta = mysqli_real_escape_string($conn, $_REQUEST['ime_prezime_pacijenta']);
 $id_pacijenta = mysqli_real_escape_string($conn, $_REQUEST['id_pacijenta']);
-$datum_pregleda = date("Y-m-d");
+$datum_pregleda = date('Y-m-d');
 $anamneza = mysqli_real_escape_string($conn, $_REQUEST['anamneza']);
 $vod = mysqli_real_escape_string($conn, $_REQUEST['vod']);
 $vos = mysqli_real_escape_string($conn, $_REQUEST['vos']);
@@ -39,15 +39,15 @@ $napomena_pregleda  = mysqli_real_escape_string($conn, $_REQUEST['napomena_pregl
 
 //Uslov u kome se provjera prosljeđena vrijednost vrsti pregleda (pregleda naočare ili sočiva).
 //Na osnovu toga se izvršava jedan od dva upita
-if ($vrsta_pregleda == "naocare") {
+if ($vrsta_pregleda == 'naocare') {
     $korekcija_daljina_od = mysqli_real_escape_string($conn, $_REQUEST['korekcija_daljina_od']);
     $korekcija_daljina_os = mysqli_real_escape_string($conn, $_REQUEST['korekcija_daljina_os']);
     $korekcija_blizina_od = mysqli_real_escape_string($conn, $_REQUEST['korekcija_blizina_od']);
     $korekcija_blizina_os = mysqli_real_escape_string($conn, $_REQUEST['korekcija_blizina_os']);
     $pd = mysqli_real_escape_string($conn, $_REQUEST['pd']);
 
-    $stmt = $conn->prepare("INSERT INTO pregledi (ID_pacijenta,ID_korisnika,ID_radnika,datum_pregleda,anamneza,vod,vos,motilitet,bms_od,bms_os,tonus_od,tonus_os,fundus_od,fundus_os,dijagnoza,terapija,korekcija_daljina_od,korekcija_daljina_os,korekcija_blizina_od,korekcija_blizina_os,pd,kontrola,napomena_pregleda,vrsta_pregleda) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("iiisssssssssssssssssssss", $id_pacijenta, $id_korisnika, $id_radnika, $datum_pregleda, $anamneza, $vod, $vos, $motilitet, $bms_od, $bms_os, $tonus_od, $tonus_os, $fundus_od, $fundus_os, $dijagnoza, $terapija, $korekcija_daljina_od, $korekcija_daljina_os, $korekcija_blizina_od, $korekcija_blizina_os, $pd, $kontrola, $napomena_pregleda, $vrsta_pregleda);
+    $stmt = $conn->prepare('INSERT INTO pregledi (ID_pacijenta,ID_korisnika,ID_radnika,datum_pregleda,anamneza,vod,vos,motilitet,bms_od,bms_os,tonus_od,tonus_os,fundus_od,fundus_os,dijagnoza,terapija,korekcija_daljina_od,korekcija_daljina_os,korekcija_blizina_od,korekcija_blizina_os,pd,kontrola,napomena_pregleda,vrsta_pregleda) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    $stmt->bind_param('iiisssssssssssssssssssss', $id_pacijenta, $id_korisnika, $id_radnika, $datum_pregleda, $anamneza, $vod, $vos, $motilitet, $bms_od, $bms_os, $tonus_od, $tonus_os, $fundus_od, $fundus_os, $dijagnoza, $terapija, $korekcija_daljina_od, $korekcija_daljina_os, $korekcija_blizina_od, $korekcija_blizina_os, $pd, $kontrola, $napomena_pregleda, $vrsta_pregleda);
 } else {
     $proizvodjac_ks_od = mysqli_real_escape_string($conn, $_REQUEST['proizvodjac_ks_od']);
     $period_ks_od = mysqli_real_escape_string($conn, $_REQUEST['period_ks_od']);
@@ -67,20 +67,20 @@ if ($vrsta_pregleda == "naocare") {
     //Na osnovu ID sočiva provjerava se rok sočiva (tj. koliko pakovanje sočiva moze da traje uz redovno korištenje)
     //Rok sočiva je več predifinisan u tabeli sočiva
     $id_sociva = mysqli_real_escape_string($conn, $_REQUEST['id_sociva']);
-    $stmt = $conn->prepare("SELECT rok FROM sociva WHERE ID =?");
-    $stmt->bind_param("i", $id_sociva);
+    $stmt = $conn->prepare('SELECT rok FROM sociva WHERE ID =?');
+    $stmt->bind_param('i', $id_sociva);
     $stmt->execute();
     $result = $stmt->get_result();
-    $rok = "";
+    $rok = '';
     while ($row = $result->fetch_object()) {
         $rok = $row->rok;
     }
 
     //Dobijeni rok se sabira sa današnjim danom i na taj način se dobija datum kada bi pacijent trebao da potroši prepisana sočiva
-    $notifikacija = date("Y-m-d", strtotime("$rok"));
+    $notifikacija = date('Y-m-d', strtotime('$rok'));
 
-    $stmt = $conn->prepare("INSERT INTO pregledi (ID_pacijenta,ID_korisnika,ID_radnika,datum_pregleda,anamneza,vod,vos,motilitet,bms_od,bms_os,tonus_od,tonus_os,fundus_od,fundus_os,dijagnoza,terapija,proizvodjac_ks_od,period_ks_od,tip_ks_od,jacina_ks_od,bc_ks_od,velicina_ks_od,boja_ks_od,proizvodjac_ks_os,period_ks_os,tip_ks_os,jacina_ks_os,bc_ks_os,velicina_ks_os,boja_ks_os,kontrola,napomena_pregleda,vrsta_pregleda,notifikacija) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("iiisssssssssssssssssssssssssssssss", $id_pacijenta, $id_korisnika, $id_radnika, $datum_pregleda, $anamneza, $vod, $vos, $motilitet, $bms_od, $bms_os, $tonus_od, $tonus_os, $fundus_od, $fundus_os, $dijagnoza, $terapija, $proizvodjac_ks_od, $period_ks_od, $tip_ks_od, $jacina_ks_od, $bc_ks_od, $velicina_ks_od, $boja_ks_od, $proizvodjac_ks_os, $period_ks_os, $tip_ks_os, $jacina_ks_os, $bc_ks_os, $velicina_ks_os, $boja_ks_os, $kontrola, $napomena_pregleda, $vrsta_pregleda, $notifikacija);
+    $stmt = $conn->prepare('INSERT INTO pregledi (ID_pacijenta,ID_korisnika,ID_radnika,datum_pregleda,anamneza,vod,vos,motilitet,bms_od,bms_os,tonus_od,tonus_os,fundus_od,fundus_os,dijagnoza,terapija,proizvodjac_ks_od,period_ks_od,tip_ks_od,jacina_ks_od,bc_ks_od,velicina_ks_od,boja_ks_od,proizvodjac_ks_os,period_ks_os,tip_ks_os,jacina_ks_os,bc_ks_os,velicina_ks_os,boja_ks_os,kontrola,napomena_pregleda,vrsta_pregleda,notifikacija) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    $stmt->bind_param('iiisssssssssssssssssssssssssssssss', $id_pacijenta, $id_korisnika, $id_radnika, $datum_pregleda, $anamneza, $vod, $vos, $motilitet, $bms_od, $bms_os, $tonus_od, $tonus_os, $fundus_od, $fundus_os, $dijagnoza, $terapija, $proizvodjac_ks_od, $period_ks_od, $tip_ks_od, $jacina_ks_od, $bc_ks_od, $velicina_ks_od, $boja_ks_od, $proizvodjac_ks_os, $period_ks_os, $tip_ks_os, $jacina_ks_os, $bc_ks_os, $velicina_ks_os, $boja_ks_os, $kontrola, $napomena_pregleda, $vrsta_pregleda, $notifikacija);
 }
 
 $stmt->execute();
--- addPatientDB.php
+++ addPatientDB.php
@@ -8,13 +8,13 @@ if (is_null($_SESSION['prijavljen'])) {
 }
 include 'connection.php';
 $korisnik = $_SESSION['prijavljen'];
-$ar = explode("#", $korisnik, 3);
-$ar[1] = rtrim($ar[1], "#");
+$ar = explode('#', $korisnik, 3);
+$ar[1] = rtrim($ar[1], '#');
 $id_korisnika = $ar[0];
 $imeKorisnika = $ar[1];
 $dataBaseName = $ar[2];
 $conn = OpenStoreCon($dataBaseName);
-mysqli_set_charset($conn, "utf8");
+mysqli_set_charset($conn, 'utf8');
 
 $imePacijenta = mysqli_real_escape_string($conn, $_REQUEST['ime_pacijenta']);
 $imeOcaPacijenta = mysqli_real_escape_string($conn, $_REQUEST['ime_oca_pacijenta']);
@@ -31,14 +31,14 @@ $sociva_os = mysqli_real_escape_string($conn, $_REQUEST['sociva_os']);
 
 //Uslov u kome se provjera da li je unijeto ime oca. Ukoliko jeste zapis generalija pacijenta će biti u formatu "Ime (Ime oca) Prezime godište"
 if (strlen($imeOcaPacijenta) > 0) {
-    $generalije_pacijenta = $imePacijenta . " (" . $imeOcaPacijenta . ") " . $prezimePacijenta . " " . $godistePacijenta;
+    $generalije_pacijenta = $imePacijenta . ' (' . $imeOcaPacijenta . ') ' . $prezimePacijenta . ' ' . $godistePacijenta;
     //Ukoliko nije, format će biti "Ime Prezime godište"
 } else {
-    $generalije_pacijenta = $imePacijenta . " " . $prezimePacijenta . " " . $godistePacijenta;
+    $generalije_pacijenta = $imePacijenta . ' ' . $prezimePacijenta . ' ' . $godistePacijenta;
 }
 
 
-$stmt = $conn->prepare("INSERT INTO pacijenti (IDKorisnika,generalije_pacijenta,kontakt,napomena,naocare_daljina_od,naocare_daljina_os,naocare_blizina_od,naocare_blizina_os,sociva_od,sociva_os) VALUES (?,?,?,?,?,?,?,?,?,?)");
+$stmt = $conn->prepare('INSERT INTO pacijenti (IDKorisnika,generalije_pacijenta,kontakt,napomena,naocare_daljina_od,naocare_daljina_os,naocare_blizina_od,naocare_blizina_os,sociva_od,sociva_os) VALUES (?,?,?,?,?,?,?,?,?,?)');
 $stmt->bind_param('isssssssss', $id_korisnika, $generalije_pacijenta, $kontaktPacijenta, $napomenaPacijenta, $naocare_daljina_od, $naocare_daljina_os, $naocare_blizina_od, $naocare_blizina_os, $sociva_od, $sociva_os);
 $stmt->execute();
 if (mysqli_error($conn)) {
