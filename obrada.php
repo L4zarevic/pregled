ce772b95642dee640ec62d061432f122c296b7d2
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-01-16 14:24:05 +0100

    Add mising value

--- obrada.php
+++ obrada.php
@@ -22,7 +22,7 @@ while ($row = $result->fetch_object()) {
     $user = $row->korisnicko_ime;
     $pass = $row->lozinka;
     $optika = $row->optika;
-    $imeKorisnika = $row->naziv;
+    $imeKorisnika = $row->pj;
     $dataBaseName = $row->db;
 }
 
