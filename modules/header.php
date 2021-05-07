6d0090dc7b7f2c84b62a9c638ec60a6709d13590
Nemanja Lazarević <36665723+L4zarevic@users.noreply.github.com>
2021-05-07 11:10:19 +0200

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -18,7 +18,7 @@ $imeKorisnika = $ar[2]; ?>
     <meta name="robots" content="noindex">
     <meta name="description" content="">
     <meta name="author" content="Nemanja Lazarevic">
-    <title>Moja Optika Stanković | e-Pregled</title>
+    <title>Moja Optika Stanković | e-Pregled</title> 
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="../pregled/css/sb-admin-2.min.css" rel="stylesheet">
